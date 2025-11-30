2c4f912dcfc2dbe74996bc76a197dbc58243f153
stropeala <petre.ispir2002@protonmail.com>
2025-12-01 00:03:04 +0200

    added user model

--- auth_project/user.py
+++ auth_project/user.py
@@ -1,5 +1,4 @@
 from flask_login import UserMixin
-
 from .db import get_db
 
 class User(UserMixin):
@@ -9,26 +8,28 @@ class User(UserMixin):
         self.email = email
         self.profile_pic = profile_pic
 
-@staticmethod
-def get(user_id):
-    db = get_db()
-    user = db.execute(
-        'SELCT * FROM user WHERE id = ?', (user_id,)
-    ).fetchone()
-    if not user:
-        return None
+    @staticmethod
+    def get(user_id):
+        db = get_db()
+        user = db.execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+        if not user:
+            return None
 
-    user = User(
-        id_ = user[0], name = user[1], email = user[2], profile_pic = user[3]
-    )
-    return user
+        user = User(
+            id_ = user[0],
+            name = user[1],
+            email = user[2],
+            profile_pic = user[3]
+        )
+        return user
 
-@staticmethod
-def create(id_, name, email, profile_pic):
-    db = get_db()
-    db.execute(
-        'INSERT INTRO user (id, name, email, profile_pic)'
-        'VALUES (?, ?, ?, ?)',
-        (id_, name, email, profile_pic),
-    )
-    db.commit()
+    @staticmethod
+    def create(id_, name, email, profile_pic):
+        db = get_db()
+        db.execute(
+            'INSERT INTO user (id, name, email, profile_pic) VALUES (?, ?, ?, ?)',
+            (id_, name, email, profile_pic),
+        )
+        db.commit()
