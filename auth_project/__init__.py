b477127c73ffb47a7e9ae267f9f9d2998cdc1f27
stropeala <petre.ispir2002@protonmail.com>
2025-12-01 00:01:13 +0200

    added __init__ flask app setup

--- auth_project/__init__.py
+++ auth_project/__init__.py
@@ -1,42 +1,29 @@
 # Python standard libraries
-import json
-import os
 import sqlite3
+import os
 
 # Third-party libs
-from flask import(
-    Flask,
-    redirect,
-    request,
-    url_for
-)
-from flask_login import(
-    LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user
-)
-from dotenv import load_dotenv
+from flask import Flask
+from flask_login import LoginManager
 from oauthlib.oauth2 import WebApplicationClient
-import requests
+from dotenv import load_dotenv
 
 # Internal imports
+from .config import GOOGLE_CLIENT_ID
 from .db import init_db
 from .user import User
+
+# Needed to work because google engorces HTTPS by default
+load_dotenv()
+insecure = os.getenv("OAUTHLIB_INSECURE_TRANSPORT")
+
+# OAuth2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 # Flask app setup
 def create_app():
     app = Flask(__name__)
-
-    # Config
-    load_dotenv()
-    app.secret_key = os.getenv('SECRET_KEY')
-    GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID', None)
-    GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET', None)
-    GOOGLE_DISCOVERY_URL = (
-        'https://accounts.google.com/.well-known/openid_configuration'
-    )
+    app.secret_key = os.getenv("SECRET_KEY")
 
     # User session management setup
     login_manager = LoginManager()
@@ -49,17 +36,14 @@ def create_app():
         except sqlite3.OperationalError:
             pass
 
-    # OAuth2 client setup
-    client = WebApplicationClient(GOOGLE_CLIENT_ID)
-
     # Flask-Login helper to retrieve a user from our db
     @login_manager.user_loader
     def load_user(user_id):
         return User.get(user_id)
 
     # Register blueprints
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    from .auth_google import auth_google as auth_google_blueprint
+    app.register_blueprint(auth_google_blueprint)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
