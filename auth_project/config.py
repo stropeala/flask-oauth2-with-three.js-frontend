e4f38344a9ccb49bf03ca3302ff45b59ae0f084c
stropeala <petre.ispir2002@protonmail.com>
2025-11-30 23:30:41 +0200

    added google config

---
+++ auth_project/config.py
@@ -0,0 +1,11 @@
+# Python standard libraries
+import os
+
+# Third-party libs
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", '')
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", '')
+GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
