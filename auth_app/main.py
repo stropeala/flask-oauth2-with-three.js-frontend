247e2a0a0a180f375bfced5a1d0c568ff063b32c
stropeala <petre.ispir2002@protonmail.com>
2025-12-03 00:08:30 +0200

    same as before (changed dir name)

---
+++ auth_app/main.py
@@ -0,0 +1,27 @@
+# Third-party libs
+from flask import Blueprint, render_template
+from flask_login import current_user
+from oauthlib.oauth2 import WebApplicationClient
+
+# Main blueprint setup
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    if current_user.is_authenticated:
+        return (
+            "<p>Hello, {}! You're logged in! Email: {}</p>"
+            '<div><p>Google Profile Picture:</p>'
+            '<img src="{}" alt="Google profile pic"></img></div>'
+            '<a class="button" href="/logout">Logout</a>'.format(
+                current_user.name,
+                current_user.email,
+                current_user.profile_pic
+            )
+        )
+    else:
+        return '<a class = "Button" href="/login">Google Login</a>'
+
+@main.route('/test')
+def test():
+    return render_template('auth_frontend/index.html', user = current_user)
